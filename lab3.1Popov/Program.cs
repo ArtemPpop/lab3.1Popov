d6eab57f970b6aacf6d96da727d4178e7f78eaf1
b.r.o.t.e.m.oo@gmail.com <b.r.o.t.e.m.oo@gmail.com>
2023-10-28 01:50:16 +0200

    Добавьте файлы проекта.

---
+++ lab3.1Popov/Program.cs
@@ -0,0 +1,18 @@
+﻿try
+{
+    Console.Write("Введите n:");
+    int n = int.Parse(Console.ReadLine());
+    double Q = 1;
+
+    for (int k = 1; k <= n; k++)
+    {
+        if (Q == Math.Pow(-1, k) * (k - 7) / (2 * (n - k)));
+        
+    }
+
+    Console.WriteLine($"Q={Q:F2}");
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
